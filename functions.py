8a244db294293f9173b311f109a57261e2356161
Vojtech Semecky <vojtech@semecky.cz>
2021-01-16 14:40:06 +0100

    Init (submodules; .gitignore; functions.py; ganimator.py)

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import sys
 
 # Allow import from git submodules
-sys.path.append("./submodules/stylegan2/")
+sys.path.append("./submodules/stylegan2-ada/")
 
 from moviepy.editor import *
 import scipy
@@ -11,5 +11,4 @@ from datetime import datetime
 import pickle
 import numpy as np
 import PIL.Image
-import pretrained_networks
 from training.misc import create_image_grid
