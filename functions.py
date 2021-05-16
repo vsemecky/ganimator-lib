463d6715890c4125fbed9a9b648a7e071cbaadce
Vojtech Semecky <vojtech@semecky.cz>
2021-05-17 01:00:50 +0200

    Code cleaning

--- functions.py
+++ functions.py
@@ -14,34 +14,21 @@ import PIL.Image
 from training.misc import create_image_grid
 
 # Memory cache for already loaded pkls
-networks_cache = {}
 networks_Gs_cache = {}
 
 
 # Loads pre-trained network from pkl file or URL
-# Result is cached for future loading the same pkl
+# Result is stored in memory cache for the future loading of the same pkl
 # @todo Remote loading using HTTP(S)
-def load_network(pkl):
-    if pkl in networks_cache.keys():
-        # Return network from cache
-        return networks_cache[pkl]
-    else:
-        # Load network from pkl file and store to cache
-        with open(pkl, 'rb') as stream:
-            print("Loading neurals: {}".format(pkl))
-            networks_cache[pkl] = pickle.load(stream, encoding='latin1')
-            return networks_cache[pkl]
-
-
 def load_network_Gs(pkl):
-    if pkl in networks_cache.keys():
-        return networks_cache[pkl]  # Return network from cache
+    if pkl in networks_Gs_cache.keys():
+        return networks_Gs_cache[pkl]  # Return network from cache
     else:
         # Load network from pkl file and store to cache
         with open(pkl, 'rb') as stream:
             print("Loading network: {}".format(pkl))
             _G, _D, Gs = pickle.load(stream, encoding='latin1')
-            networks_cache[pkl] = Gs
+            networks_Gs_cache[pkl] = Gs
             return Gs
 
 
@@ -101,7 +88,6 @@ def generate_image(pkl: str, seed: int = 42, trunc: float = None, randomize_nois
     if trunc is not None:
         Gs_kwargs.truncation_psi = trunc
 
-    print(f'Generating image (seed={seed}, trunc={trunc})')
     rnd = np.random.RandomState(seed)
     z = rnd.randn(1, *Gs.input_shape[1:])  # [minibatch, component]
     tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars})  # [height, width]
